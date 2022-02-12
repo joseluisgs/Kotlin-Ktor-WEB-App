9851b90e7a97a160676ea690be09ec180b342498
joseuisgs <joseluisgonsan@gmail.com>
2022-02-12 10:37:38 +0100

    Mustache configuracion y ejemplo

--- src/main/kotlin/es/joseluisgs/Application.kt
+++ src/main/kotlin/es/joseluisgs/Application.kt
@@ -1,6 +1,9 @@
 package es.joseluisgs
 
+import com.github.mustachejava.DefaultMustacheFactory
+import es.joseluisgs.models.User
 import io.ktor.application.*
+import io.ktor.mustache.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.cio.*
@@ -10,6 +13,9 @@ fun main(args: Array<String>) = EngineMain.main(args)
 fun Application.module() {
     val presentacion = environment.config.propertyOrNull("mensajes.presentacion")?.getString() ?: "Hola Kotlin"
     val mode = environment.config.property("ktor.environment").getString()
+
+    // Iniciamos Mustache
+    initMustache()
 
     // Comenzamos a registrar las rutas
     initRoutes(presentacion, mode)
@@ -25,5 +31,21 @@ private fun Application.initRoutes(presentacion: String, mode: String) {
         get("/test") {
             call.respondText("$presentacion en modo $mode")
         }
+        get("/user") {
+            val user = User(1, "Pepe", "Perez")
+            val data = mapOf("user" to user, "title" to "Ktor con Mustache")
+            call.respond(MustacheContent("user.hbs", data))
+        }
+    }
+}
+
+
+/**
+ * Inicializamos el motor de plantillas
+ */
+private fun Application.initMustache() {
+    install(Mustache) {
+        // Indicamos donde está el directorio de plantillas
+        mustacheFactory = DefaultMustacheFactory("templates")
     }
 }
---
+++ src/main/kotlin/es/joseluisgs/models/User.kt
@@ -0,0 +1,3 @@
+package es.joseluisgs.models
+
+data class User(val id: Int, val firstName: String, val lastName: String)
